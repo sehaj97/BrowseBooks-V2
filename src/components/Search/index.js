4c1f8e7687b15ea1507cc05a2586beadb80f8075
sehaj97 <sehajmagan@gmail.com>
2022-03-14 18:30:57 -0400

    added mobile fix

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -6,7 +6,7 @@ const DropDown = ({toggle, orderBy, onOrderByChange, sortBy, onSortByChange}) =>
         return null
     }
     return (    <div className="origin-top-right absolute mt-2 w-56
-    rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+    rounded-md shadow-lg bg-white ring-1 ring-black right-0 sm:right-auto ring-opacity-5">
     <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
       <div onClick={()=> {onSortByChange('title')}} tabIndex='0'
         className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
